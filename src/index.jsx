039dc8431b6f6ed543ea9c9963927bd86945c251
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-09 01:41:34 +0530

    Delete src directory

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-// src/index.js
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App"; // but App.js must also avoid JSX
-
-createRoot(document.getElementById("root")).render(
-  React.createElement(App)
-);
