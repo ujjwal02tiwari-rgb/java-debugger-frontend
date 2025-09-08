366d08e3df6b9c080381124e92804802f0c315bd
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-08 23:42:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+// src/index.js
 import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App"; // but App.js must also avoid JSX
 
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+createRoot(document.getElementById("root")).render(
+  React.createElement(App)
+);
